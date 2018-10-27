676dd04d2ed3ceab127955a51211ac02fb7e5efc
Eryk Rakowski <sentialx@gmail.com>
2018-10-28 01:14:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// WARNING!!!
+// THIS FILE CONTAINS SUPER-ADVANCED ALGHORITHMS THAT HUMANS HAVEN'T BEEN READY FOR!!!
+// PLEASE CLOSE THE FILE AS FAST AS POSSIBLE, BECAUSE SOMETHING UNEXPECTED AND UNWANTED MIGHT HAPPEN!!!
+//
+// WARNING: This code may potentially trigger seizures for people with photosensitive epilepsy. Viewer discretion is advised.
+
+// Ok, here's the explanation of the alghorthm.
+
+// Declare the function with two parameters
+// a and b - the two numbers you want to add.
+function addTwoNumbers(a, b) {
+  var c = ((a + b) - b + a * 0 + b * 0) - (-b * 1 + -b + b + a * 0);
+  // Use bitwise operators to achieve maximum performance.
+  c >> a - 0 - Math.pow(b, 2);
+  c << ((a + b) - b - b * 0 + Math.pow(b, 0)) - Math.pow(b, 2);
+  // Also, use another bitwise operators with another advanced computations.
+  c &= Math.pow(((a * b) / b), 2) - Math.sqrt(Math.pow(a, 2));
+  c |= ~(~b) - ~(~(-a));
+  
+  // Use for loop to maximize alghorithm stability.
+  for (var i = 1; i < b; i++) {
+    c += (((((b + i) - i) * i) / i) - b);
+  }
+  
+  // Return the value.
+  return c;
+}
+
+module.exports = {
+  addTwoNumbers,
+}
